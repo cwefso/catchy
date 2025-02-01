0371038bb175857026c46e9d65446d27a71b260b
Charles Wefso <61127453+cwefso@users.noreply.github.com>
2025-01-31 17:39:59 -0700

    use env

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const SPOTIFY_CLIENT_ID = "fa3471a644cd49c3bd14a32285f8b5b6";
-const SPOTIFY_CLIENT_SECRET = "b904fa989d9e4deab12060dd4104ef08";
-const SPOTIFY_REDIRECT_URI = "http://localhost:3000/callback/";
+const SPOTIFY_CLIENT_ID = `${process.env.SPOTIFY_CLIENT_ID}`;
+const SPOTIFY_CLIENT_SECRET = `${process.env.SPOTIFY_CLIENT_SECRET}`;
+const SPOTIFY_REDIRECT_URI = `${process.env.SPOTIFY_REDIRECT_URI}`;
 
 // Generate the authorization URL
 export const getAuthorizationUrl = () => {
